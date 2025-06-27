8932aee32695b5521b872bedd3c0828bcf9e4af7
Michael Harris <158084149+Mikemike8@users.noreply.github.com>
2025-06-27 15:06:57 -0500

    chat bot

--- Jreesor-frontend/src/App.jsx
+++ Jreesor-frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {  useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Nav } from './components/Nav';
@@ -12,6 +12,9 @@ import { useLocation } from 'react-router-dom';
 
 
 function App() {
+      useEffect(() => {(function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...args)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(args)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="g2FhobLszRRQR3leSt02t";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})()}, []); // runs only once when component mounts
+  
+  
      const location = useLocation();
 
   const isPlacePage = location.pathname === '/place';
